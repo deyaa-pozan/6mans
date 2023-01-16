c21209959c8ee9e1790d9673d513ff45ae210264
deyaa-pozan <deyaa.pozan@gmail.com>
2023-01-17 02:06:31 +0300

    6mans discord.js v-^14.7.1

---
+++ utils/commands.js
@@ -0,0 +1,20 @@
+exports.commandToString = {
+  queue: '!6m-q',
+  leave: '!6m-leave',
+  status: '!6m-status',
+  votestatus: '!6m-votestatus',
+  r: '!6m-r',
+  c: '!6m-c',
+  help: '!6help',
+  kick: '!6m-kick',
+}
+
+exports.validCommandCheck = {
+  '!6m-q': true,
+  '!6m-leave': true,
+  '!6m-status': true,
+  '!6m-votestatus': true,
+  '!6m-r': true,
+  '!6m-c': true,
+  '!6m-kick': true,
+}
